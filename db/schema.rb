2b4cec03af7355a98f8d0e06fea8aa5063b8c431
Vincent REHM <vincentrehm@yahoo.fr>
2018-10-06 23:51:31 +0200

    scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_06_212945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "director"
+    t.text "storyline"
+    t.date "watched_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+
+100.times do |index|
+  Movie.create!(title: Faker::Lorem.sentence(3, false, 0).chop,
+                director: Faker::Name.name,
+                storyline: Faker::Lorem.paragraph,
+                watched_on: Faker::Time.between(4.months.ago, 1.week.ago))
+end
+
+p "Created #{Movie.count} movies"
